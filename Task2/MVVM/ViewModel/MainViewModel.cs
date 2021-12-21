d2b569b3838c2a9c614dd1a08acf0bb0427a557f
Sadi_er59 <Sadi_er59@STHQ0118-15>
2021-12-21 11:57:57 +0400

    Add project files.

---
+++ Task2/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.MVVM.ViewModel
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public MainViewModel(MainWindow mainWindow)
+        {
+            var ipAdress = IPAddress.Parse("10.1.18.29");
+            var port = 27001;
+            using(var socket=new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                var ep = new IPEndPoint(ipAdress, port);
+                socket.Bind(ep);
+                socket.Listen(10);
+                while (true)
+                {
+                    var client = socket.Accept();
+                    Task.Run(() =>
+                    {
+                        var length = 0;
+                        var bytes = new byte[1024];
+                        do
+                        {
+                            length = client.Receive(bytes);
+                            var message = Encoding.UTF8.GetString(bytes, 0, length);
+                            mainWindow.ImagesListBox.Items.Add(message);
+                        } while (true);
+                    });
+                }
+            }
+        }
+    }
+}
